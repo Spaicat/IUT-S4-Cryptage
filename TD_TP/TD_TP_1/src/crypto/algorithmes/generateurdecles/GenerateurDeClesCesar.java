4d51e162131a8d13dc4f41f9cb1c070c36df9abe
0rega <72932562+0rega@users.noreply.github.com>
2021-01-26 20:48:27 +0100

    Aban-dons
    Création classe generateurDeCleCesar
    fonction générerClePublique
    
    FIN
    
    Pas du tout compris comment on est censé générer la clé pv

---
+++ TD_TP/TD_TP_1/src/crypto/algorithmes/generateurdecles/GenerateurDeClesCesar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crypto.algorithmes.generateurdecles;
+
+import crypto.donnees.cles.Cle;
+import crypto.donnees.cles.CleInteger;
+import crypto.donnees.cles.Cles;
+import java.util.Random;
+
+/**
+ *
+ * @author asus
+ */
+public class GenerateurDeClesCesar {
+    
+    public GenerateurDeClesCesar(){
+        
+    }
+    
+    //l'algorithme n'a pas de cle publique
+    public Cles genererClePublique(){
+        return null;
+    }
+    
+    public Cles genererClePrivee(){
+        return null; //pas compris mdr
+    }
+    
+}
