9ceab507dd90fc6f55652deccfbf5cc58ab53901
0rega <72932562+0rega@users.noreply.github.com>
2021-01-27 18:50:06 +0100

    Jusqu'à Q14

--- TD_TP/TD_TP_1/src/crypto/algorithmes/chiffrement/AlgorithmeTransposition.java
+++ TD_TP/TD_TP_1/src/crypto/algorithmes/chiffrement/AlgorithmeTransposition.java
@@ -11,6 +11,10 @@ import crypto.donnees.messages.Message;
 import crypto.exceptions.ExceptionConversionImpossible;
 import crypto.exceptions.ExceptionCryptographie;
 import static java.lang.Math.ceil;
+import static java.lang.Math.random;
+import static java.lang.StrictMath.random;
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
 
 /**
  *
@@ -18,6 +22,7 @@ import static java.lang.Math.ceil;
  */
 public class AlgorithmeTransposition implements Algorithme{
 
+    SecureRandom generateur;
     @Override
     public String getNom() {
         return "Transposition";
@@ -32,6 +37,10 @@ public class AlgorithmeTransposition implements Algorithme{
      */
     private char[][] remplirTableauChiffrement(Message message,Cle cle)throws ExceptionConversionImpossible{
        
+        ByteBuffer b = ByteBuffer.allocate(4) ;
+        b.putInt((cle.asString()+message.asString()).hashCode()) ;
+        this.generateur = new SecureRandom (b.array()) ;
+
         //Initilisation tableau ligne
         int tailleTableauX = cle.asString().length();
         
@@ -54,7 +63,8 @@ public class AlgorithmeTransposition implements Algorithme{
                 //colonne
                 for(int x = 0;x < tailleTableauX;x++){
                     
-                    char temp = 'x';
+                    //Choisit une valeur aléatoire et elle s'ajoutera au tableau que si i > tailleMsg
+                    char temp = bourrage();
                     
                     //On verifie que i est bien inférieur à la taille du message
                     if(i < tailleMsg)
@@ -70,6 +80,24 @@ public class AlgorithmeTransposition implements Algorithme{
 
         return tab;
              
+    }
+    
+    /**
+     * 
+     * @return un caractere au hasard
+     */
+    private char bourrage(){
+        
+        //String avec toutes les lettres
+        String allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefeghijklmnopqrstuvwxyz";
+        
+        //On choisit une position au hasard
+        int randomPos = this.generateur.nextInt(allLetters.length());
+        
+        //On récupère le caractere à la position indiquée
+        char randomChar = allLetters.charAt(randomPos);
+        
+        return randomChar;
     }
 
     @Override
