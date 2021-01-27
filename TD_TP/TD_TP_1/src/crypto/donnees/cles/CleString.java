370402a81655c41db25a7b0085df2dafb1592e91
Thibault <55053185+Spaicat@users.noreply.github.com>
2021-01-27 15:58:15 +0100

    Create CleString.java

---
+++ TD_TP/TD_TP_1/src/crypto/donnees/cles/CleString.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crypto.donnees.cles;
+
+import crypto.exceptions.ExceptionConversionImpossible;
+
+/**
+ *
+ * @author Thibault
+ */
+public class CleString implements Cle {
+
+    @Override
+    public String asString() throws ExceptionConversionImpossible {
+        return null;
+    }
+
+    @Override
+    public int asInteger() throws ExceptionConversionImpossible {
+        return null;
+    }
+    
+}
