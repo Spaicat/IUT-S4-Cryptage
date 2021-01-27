4c3fe8cf1d0c82dd6f46d1ec3b0c6bfc24671ea4
0rega <72932562+0rega@users.noreply.github.com>
2021-01-27 17:41:49 +0100

    Tableau de merde

--- TD_TP/TD_TP_1/src/crypto/algorithmes/chiffrement/AlgorithmeTransposition.java
+++ TD_TP/TD_TP_1/src/crypto/algorithmes/chiffrement/AlgorithmeTransposition.java
@@ -5,10 +5,60 @@
  */
 package crypto.algorithmes.chiffrement;
 
+import crypto.donnees.cles.Cle;
+import crypto.donnees.cles.Cles;
+import crypto.donnees.messages.Message;
+import crypto.exceptions.ExceptionConversionImpossible;
+import crypto.exceptions.ExceptionCryptographie;
+import static java.lang.Math.ceil;
+
 /**
  *
  * @author asus
  */
-public class AlgorithmeTransposition {
+public class AlgorithmeTransposition implements Algorithme{
+
+    @Override
+    public String getNom() {
+        return "Transposition";
+    }
+    
+    private char[][] remplirTableauChiffrement(Message message,Cle cle)throws ExceptionConversionImpossible{
+                
+        int tailleTableauX = cle.asString().length();
+        int tailleMsg = message.asString().length();
+        int i = 0;
+        //Connaitre le nombre de ligne
+        int calcul = tailleMsg / tailleTableauX;
+        int tailleTableauY = (int)ceil(calcul);
+        
+        char [][] tab = new char[tailleTableauX][tailleTableauY];
+              
+            //ligne
+            for(int y = 0;y < tailleTableauY;y++){
+                //colonne
+                for(int x = 0;x < tailleTableauX;x++){
+                                      
+                        char temp = message.asString().charAt(i);
+                        tab[x][y] = temp;
+                        i++;
+                        System.out.println(temp);
+                        
+                    }
+                }
+
+        return tab;
+             
+    }
+
+    @Override
+    public Message chiffrer(Message message, Cles clesPubliques, Cles clesPrivees) throws ExceptionCryptographie {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Message dechiffrer(Message message, Cles clesPubliques, Cles clesPrivees) throws ExceptionCryptographie {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
---
+++ TD_TP/TD_TP_1/test/crypto/algorithmes/chiffrement/RemplirTableauChiffrementTest.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crypto.algorithmes.chiffrement;
+
+import crypto.donnees.cles.Cle;
+import crypto.donnees.cles.CleString;
+import crypto.donnees.cles.Cles;
+import crypto.donnees.messages.Message;
+import crypto.donnees.messages.MessageString;
+import java.lang.reflect.Method;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author simonetma
+ */
+public class RemplirTableauChiffrementTest {
+    
+    
+
+    
+
+    /**
+     * Test of dechiffrer method, of class AlgorithmeTransposition.
+     */
+    @Test
+    public void testRemplirTableauSansBourrage() throws Exception {
+        System.out.println("Test du remplissage du tableau sans bourrage");
+        Message message = new MessageString("Bonjour comment ca va ?");
+        Cle cleString = new CleString("Chat");
+        
+        AlgorithmeTransposition instance = new AlgorithmeTransposition();
+        Method methode = instance.getClass().getDeclaredMethod("remplirTableauChiffrement", Message.class,Cle.class);
+        methode.setAccessible(true);
+        char[][] result = (char[][]) methode.invoke(instance, message,cleString);
+        
+        char[][] expResult = {
+            {'B','o','n','j'},
+            {'o','u','r',' '},
+            {'c','o','m','m'},
+            {'e','n','t',' '},
+            {'c','a',' ','v'},
+            {'a',' ','?','x'},
+        };
+        
+        assertArrayEquals(expResult,result);
+    }
+    
+    /**
+     * Test of dechiffrer method, of class AlgorithmeTransposition.
+     */
+    @Test
+    public void testRemplirTableauSansBourrage2() throws Exception {
+        System.out.println("Test du remplissage du tableau sans bourrage");
+        Message message = new MessageString("Bonjour, comment ca va ?");
+        Cle cleString = new CleString("Chat");
+        
+        AlgorithmeTransposition instance = new AlgorithmeTransposition();
+        Method methode = instance.getClass().getDeclaredMethod("remplirTableauChiffrement", Message.class,Cle.class);
+        methode.setAccessible(true);
+        char[][] result = (char[][]) methode.invoke(instance, message,cleString);
+        
+        char[][] expResult = {
+            {'B','o','n','j'},
+            {'o','u','r',','},
+            {' ','c','o','m'},
+            {'m','e','n','t'},
+            {' ','c','a',' '},
+            {'v','a',' ','?'},
+        };
+        
+        assertArrayEquals(expResult,result);
+    }
+}
