5a3bacf83605e9c94833acd810f45fffc7c2cfe0
Thibault <55053185+Spaicat@users.noreply.github.com>
2021-01-28 15:18:44 +0100

    Ajout javadoc Univers

--- TD_TP/TD_TP_1/src/crypto/entites/Univers.java
+++ TD_TP/TD_TP_1/src/crypto/entites/Univers.java
@@ -10,8 +10,8 @@ import crypto.donnees.messages.Message;
 import java.util.HashMap;
 
 /**
- *
- * @author asus
+ * Modelise tout ce qui est public
+ * @author Thibault, Antonia
  */
 public class Univers {
     private HashMap<String, Cles> listeClesPubliques;
@@ -23,22 +23,46 @@ public class Univers {
         this.listeMessagesPublics = new HashMap<String, Message>();
     }
     
+    /**
+     * Permet d'obtenir l'instance de la classe
+     * @return l'instance de la classe
+     */
     public static Univers get() {
         return instance;
     }
     
+    /**
+     * Ajoute les clés dans la liste des cles publiques de notre Univers
+     * @param identifiant identifiant qui permet de representer les clés
+     * @param cles les cles à ajouter
+     */
     public static void addCles(String identifiant, Cles cles) {
         instance.listeClesPubliques.put(identifiant, cles);
     }
     
+    /**
+     * Permet d'obtenir les cles publiques de notre Univers
+     * @param identifiant identifiant des clés cherchés
+     * @return le message qu'on veut obtenir
+     */
     public static Cles getCles(String identifiant) {
         return instance.listeClesPubliques.get(identifiant);
     }
     
+    /**
+     * Ajoute les messages dans la liste des messages publiques de notre Univers
+     * @param identifiant identifiant qui permet de representer le message
+     * @param message le message qu'on veut ajouter
+     */
     public static void addMessage(String identifiant, Message message) {
         instance.listeMessagesPublics.put(identifiant, message);
     }
     
+    /**
+     * Permet d'obtenir les messages publiques de notre Univers
+     * @param identifiant identifiant du message cherché
+     * @return le message qu'on veut obtenir
+     */
     public static Message getMessage(String identifiant) {
         return instance.listeMessagesPublics.get(identifiant);
     }
