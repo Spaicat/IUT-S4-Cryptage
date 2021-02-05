182e690cd0e299923d138016ca6f7e42ceb27c6b
0rega <72932562+0rega@users.noreply.github.com>
2021-02-05 12:17:48 +0100

    Update AlgorithmeSubstitution.java

--- TD_TP/Crypto_nite/src/crypto/algorithmes/chiffrement/AlgorithmeSubstitution.java
+++ TD_TP/Crypto_nite/src/crypto/algorithmes/chiffrement/AlgorithmeSubstitution.java
@@ -25,8 +25,11 @@ public class AlgorithmeSubstitution implements Algorithme{
 
     @Override
     public Message chiffrer(Message message, Cles clesPubliques, Cles clesPrivees) throws ExceptionCryptographie {
-                  
-   
+        
+        //Message crypté
+        String messageCrypte = "";
+            
+        try{
             //Alphabet basique
             char[] tabAlpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
             char[] tabCle = new char[26];
@@ -44,23 +47,34 @@ public class AlgorithmeSubstitution implements Algorithme{
             //Char crypté
             char[] charCrypte = new char[tailleMsg];
             
-            //Message crypté
-            String messageCrypte = "";
+            
         
             //On chiffre le message
             for (int i = 0; i < tailleMsg; i++)
             {
-                for (int j = 0; j < 26; j++)
-                {
-                    if (tabAlpha[j] == message.asString().charAt(i))
+                char c;
+                if((int)message.asString().charAt(i) < 65 || (int)message.asString().charAt(i) > 90){
+                    c = message.asString().charAt(i);
+                    messageCrypte += c;
+                } 
+                else{
+                    for (int j = 0; j < 26; j++)
                     {
-                        charCrypte[i] = tabCle[j];
-                        char c = charCrypte[i];
-                         messageCrypte += c;
+                        if (tabAlpha[j] == message.asString().charAt(i))
+                        {
+                            charCrypte[i] = tabCle[j];
+                            c = charCrypte[i];
+                            messageCrypte += c;
+                        }
+
                     }
-                    
                 }
+                
             }
+        }
+        catch(Exception e){
+            throw new ExceptionAlgorithmeNonDefini("Algorithme non defini");
+        }
             
             message = new MessageString(messageCrypte);
               
